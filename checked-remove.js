eee9887407bba0723b1d806d8bb6a75542fb9a88
Wellington Gallo <wellington.gallo@fluig.com>
2020-02-12 16:22:46 -0300

    ajuste

--- checked-remove.js
+++ checked-remove.js
@@ -3,8 +3,9 @@ var mergeButton = document.querySelector('.aui-button-primary.merge-pull-request
 if (mergeButton) {
   document.querySelector('.aui-button-primary.merge-pull-request').addEventListener('click', function() {
     let getElement = document.querySelector('#delete-branch-checkbox');
+    let refBranch = document.querySelector('.ref.branch').textContent;
     
-    if (getElement) {
+    if (getElement && (refBranch !== "develop" || refBranch !== "staging" || refBranch !== "master")) {
       getElement.checked = true;
     }
   });
